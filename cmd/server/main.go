fbb9540882ea3a50bd04418c1120ba403c4d5033
Diego <x@diego.pizza>
2019-05-20 15:56:34 +0100

    adds certificates and server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,128 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"bitbucket.org/openbankingteam/conformance-suite/pkg/tracer"
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+	"gopkg.in/resty.v1"
+)
+
+const (
+	certFile = "./configs/dcr.crt"
+	keyFile  = "./configs/dcr.key"
+)
+
+var (
+	logger  = logrus.StandardLogger()
+	rootCmd = &cobra.Command{
+		Use:   "dcr_server",
+		Short: "Dynamic Client Registration Conformance Suite",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			logger := logger.WithField("app", "server")
+			server := echo.New()
+			address := fmt.Sprintf("%s:%d", viper.GetString("host"), viper.GetInt("port"))
+			logger.Infof("listening on https://%s", address)
+			return server.StartTLS(address, certFile, keyFile)
+		},
+	}
 )
 
 func main() {
-	fmt.Printf("Dynamic Client Registration Conformance Tool server")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprint(os.Stderr, err)
+		fmt.Fprint(os.Stderr, "\n")
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().String("log_level", "INFO", "Log level")
+	rootCmd.PersistentFlags().Bool("log_tracer", false, "Enable tracer logging")
+	rootCmd.PersistentFlags().Bool("log_http_trace", false, "Enable HTTP logging")
+	rootCmd.PersistentFlags().Int("port", 8443, "Server port")
+	rootCmd.PersistentFlags().String("host", "127.0.0.1", "Server host")
+	rootCmd.PersistentFlags().Bool("disable_jws", false, "Disable JWS Signature")
+
+	if err := viper.BindPFlags(rootCmd.PersistentFlags()); err != nil {
+		fmt.Fprint(os.Stderr, err)
+		fmt.Fprint(os.Stderr, "\n")
+		os.Exit(1)
+	}
+
+	viper.SetEnvPrefix("")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	logger.SetNoLock()
+	logger.SetFormatter(&prefixed.TextFormatter{
+		DisableColors:    false,
+		ForceColors:      true,
+		TimestampFormat:  time.RFC3339,
+		FullTimestamp:    true,
+		DisableTimestamp: false,
+		ForceFormatting:  true,
+	})
+	level, err := logrus.ParseLevel(viper.GetString("log_level"))
+	if err != nil {
+		printConfigurationFlags()
+		fmt.Fprint(os.Stderr, err)
+		fmt.Fprint(os.Stderr, "\n")
+		os.Exit(1)
+	}
+	logger.SetLevel(level)
+
+	tracer.Silent = !viper.GetBool("log_tracer")
+	if viper.GetBool("log_to_file") {
+		f, err := os.OpenFile("suite.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			// continue as normal
+		} else {
+			mw := f // io.MultiWriter(os.Stdout, f)
+			logrus.SetOutput(mw)
+			logger.SetFormatter(&prefixed.TextFormatter{
+				DisableColors:    true,
+				ForceColors:      false,
+				TimestampFormat:  time.RFC3339,
+				FullTimestamp:    true,
+				DisableTimestamp: false,
+				ForceFormatting:  true,
+			})
+
+		}
+	}
+	if viper.GetBool("log_http_file") {
+		httpLogFile, err := os.OpenFile("http-trace.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			logrus.Warn("cannot set http trace file")
+		} else {
+			resty.SetLogger(httpLogFile)
+		}
+	}
+
+	resty.SetDebug(viper.GetBool("log_http_trace"))
+
+	printConfigurationFlags()
+}
+
+func printConfigurationFlags() {
+	logger.WithFields(logrus.Fields{
+		"log_level":      viper.GetString("log_level"),
+		"log_tracer":     viper.GetBool("log_tracer"),
+		"log_http_trace": viper.GetBool("log_http_trace"),
+		"log_http_file":  viper.GetBool("log_http_file"),
+		"log_to_file":    viper.GetBool("log_to_file"),
+		"port":           viper.GetInt("port"),
+		"tracer.Silent":  tracer.Silent,
+	}).Info("configuration flags")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module bitbucket.org/openbankingteam/conformance-dcr
 go 1.12
 
 require (
+	bitbucket.org/openbankingteam/conformance-suite v1.1.7
 	github.com/golangci/golangci-lint v1.16.0 // indirect
+	github.com/labstack/echo v3.2.1+incompatible
+	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.4.0
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/viper v1.3.1
+	github.com/x-cray/logrus-prefixed-formatter v0.5.2
 	golang.org/x/tools v0.0.0-20190517003510-bffc5affc6df // indirect
+	gopkg.in/resty.v1 v1.12.0
 )
