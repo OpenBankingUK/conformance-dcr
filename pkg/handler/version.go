d48d9e4987b3b0b491e0c63a0065454909b39b3c
Diego <x@diego.pizza>
2019-05-21 10:01:38 +0100

    adds some doc

--- pkg/handler/version.go
+++ pkg/handler/version.go
@@ -6,14 +6,18 @@ import (
 	"github.com/labstack/echo"
 )
 
+// VersionHandler is used to expose the version to the version endpoint
 type VersionHandler struct {
 	version string
 }
 
+// NewVersionHandler returns an instance of VersionHandler
+// it initialises it with the version provided
 func NewVersionHandler(version string) VersionHandler {
 	return VersionHandler{version}
 }
 
+// ShowVersion is the handler for GET /version
 func (v VersionHandler) ShowVersion(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{"version": v.version})
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -9,11 +9,15 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// Server decorates a standard echo.Echo server instance
+// it may have additional configuration relating to this application instance
 type Server struct {
 	*echo.Echo
 	Version string
 }
 
+// NewServer returns a new instance of Server
+// it uses a logger middleware using the logger instance provided
 func NewServer(echoSrv *echo.Echo, logger *logrus.Entry, version string) Server {
 	srv := Server{echoSrv, version}
 	srv.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
