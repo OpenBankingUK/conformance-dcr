a41e4d86aca61e186265beee9a0c999981abef34
Diego <x@diego.pizza>
2019-05-21 10:16:29 +0100

    removed global vars

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
+	"bitbucket.org/openbankingteam/conformance-dcr/pkg/server"
 	"fmt"
+	"github.com/labstack/echo"
+	"github.com/spf13/cobra"
 	"os"
 	"strings"
 	"time"
 
-	"github.com/labstack/echo"
 	"github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 	"gopkg.in/resty.v1"
-
-	"bitbucket.org/openbankingteam/conformance-dcr/pkg/server"
 )
 
 const (
@@ -23,24 +22,63 @@ const (
 	host     = "127.0.0.1"
 )
 
-var (
-	logger = logrus.StandardLogger()
-)
+type ConfigBuilder struct {
+	logger *logrus.Logger
+}
 
-func main() {
-	rootCmd := initRootCmd()
-	if err := rootCmd.Execute(); err != nil {
+func NewConfigBuilder(logger *logrus.Logger) ConfigBuilder {
+	return ConfigBuilder{logger}
+}
+
+func (cfgBuilder ConfigBuilder) InitConfig() {
+	cfgBuilder.logger.SetNoLock()
+	cfgBuilder.logger.SetFormatter(&prefixed.TextFormatter{
+		DisableColors:    false,
+		ForceColors:      true,
+		TimestampFormat:  time.RFC3339,
+		FullTimestamp:    true,
+		DisableTimestamp: false,
+		ForceFormatting:  true,
+	})
+	level, err := logrus.ParseLevel(viper.GetString("log_level"))
+	if err != nil {
+		cfgBuilder.PrintConfigFlags()
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	cfgBuilder.logger.SetLevel(level)
+	if viper.GetBool("log_http_file") {
+		httpLogFile, err := os.OpenFile("http-trace.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			logrus.Warn("cannot set http trace file")
+		} else {
+			resty.SetLogger(httpLogFile)
+		}
+	}
+
+	resty.SetDebug(viper.GetBool("log_http_trace"))
+
+	cfgBuilder.PrintConfigFlags()
+}
+
+func (cfgBuilder ConfigBuilder) PrintConfigFlags() {
+	cfgBuilder.logger.WithFields(logrus.Fields{
+		"log_level":      viper.GetString("log_level"),
+		"log_tracer":     viper.GetBool("log_tracer"),
+		"log_http_trace": viper.GetBool("log_http_trace"),
+		"log_http_file":  viper.GetBool("log_http_file"),
+		"log_to_file":    viper.GetBool("log_to_file"),
+		"port":           viper.GetInt("port"),
+	}).Info("configuration flags")
 }
 
 func initRootCmd() *cobra.Command {
+	configBuilder := NewConfigBuilder(logrus.StandardLogger())
 	rootCmd := &cobra.Command{
 		Use:   "dcr_server",
 		Short: "Dynamic Client Registration Conformance Suite",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			logger := logger.WithField("app", "server")
+			logger := configBuilder.logger.WithField("app", "server")
 			server := server.NewServer(echo.New(), logger, version)
 			address := fmt.Sprintf("%s:%d", host, viper.GetInt("port"))
 			logger.Infof("listening on https://%s", address)
@@ -61,49 +99,15 @@ func initRootCmd() *cobra.Command {
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
 
-	cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(configBuilder.InitConfig)
 
 	return rootCmd
 }
 
-func initConfig() {
-	logger.SetNoLock()
-	logger.SetFormatter(&prefixed.TextFormatter{
-		DisableColors:    false,
-		ForceColors:      true,
-		TimestampFormat:  time.RFC3339,
-		FullTimestamp:    true,
-		DisableTimestamp: false,
-		ForceFormatting:  true,
-	})
-	level, err := logrus.ParseLevel(viper.GetString("log_level"))
-	if err != nil {
-		printConfigurationFlags()
+func main() {
+	rootCmd := initRootCmd()
+	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	logger.SetLevel(level)
-	if viper.GetBool("log_http_file") {
-		httpLogFile, err := os.OpenFile("http-trace.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-		if err != nil {
-			logrus.Warn("cannot set http trace file")
-		} else {
-			resty.SetLogger(httpLogFile)
-		}
-	}
-
-	resty.SetDebug(viper.GetBool("log_http_trace"))
-
-	printConfigurationFlags()
-}
-
-func printConfigurationFlags() {
-	logger.WithFields(logrus.Fields{
-		"log_level":      viper.GetString("log_level"),
-		"log_tracer":     viper.GetBool("log_tracer"),
-		"log_http_trace": viper.GetBool("log_http_trace"),
-		"log_http_file":  viper.GetBool("log_http_file"),
-		"log_to_file":    viper.GetBool("log_to_file"),
-		"port":           viper.GetInt("port"),
-	}).Info("configuration flags")
 }
