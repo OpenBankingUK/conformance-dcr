371abc240e59cd64924545c218eb195874d01037
Diego <x@diego.pizza>
2019-05-24 11:22:30 +0100

    adds basic logic to validate a jwt

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module bitbucket.org/openbankingteam/conformance-dcr
 go 1.12
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.2.8 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
--- pkg/ssa/ssa_validator.go
+++ pkg/ssa/ssa_validator.go
@@ -1,6 +1,13 @@
 package ssa
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 // The SSA header MUST comply with [RFC7519]
 // Signing algorithms MUST be PS256 or ES256
@@ -9,18 +16,60 @@ const (
 	SigningES256 = "ES256"
 )
 
+type SSAValidator struct {
+	pubKeyLookup func(t *jwt.Token) (interface{}, error)
+	parser       jwt.Parser
+}
+
+func NewSSAValidator(pubKeyLookup func(t *jwt.Token) (interface{}, error)) SSAValidator {
+	return SSAValidator{
+		pubKeyLookup: pubKeyLookup,
+		parser:       jwt.Parser{ValidMethods: []string{SigningPS256, SigningES256}},
+	}
+}
+
+func PublicKeyLookupFromByteSlice(pubKey []byte) func(t *jwt.Token) (interface{}, error) {
+	return func(t *jwt.Token) (interface{}, error) {
+		return jwt.ParseRSAPublicKeyFromPEM(pubKey)
+	}
+}
+
+func PublicKeyLookupFromJWKSEndpoint() func(t *jwt.Token) (interface{}, error) {
+	return func(t *jwt.Token) (interface{}, error) {
+		tkmap, ok := t.Claims.(jwt.MapClaims)
+		if !ok {
+			return nil, errors.New("unable to cast token claim to map[string]interface{}")
+		}
+		jwkEndpoint, ok := tkmap["software_jwks_endpoint"].(string)
+		if !ok {
+			return nil, errors.New("unable to cast jwk endpoint to string")
+		}
+		res, err := http.Get(jwkEndpoint)
+		if err != nil {
+			return nil, err
+		}
+		var jwk map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&jwk)
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("unable to parse json from jwk endpoint %s err: %v", jwkEndpoint, err))
+		}
+		// todo
+		// lookup for x5u with kid == tkmap["kid"]
+		return jwt.ParseRSAPublicKeyFromPEM([]byte(``))
+	}
+}
+
 // Validate gets a software statement assertion as a jwt
 // parses and validates that the jwt is valid
 // returns a valid SSA struct
-func Validate(ssa string) (SSA, error) {
-	parser := jwt.Parser{
-		ValidMethods: []string{SigningPS256, SigningES256},
-	}
-	t, err := parser.Parse(ssa, func(t *jwt.Token) (interface{}, error) {
-		return 0, nil
-	})
+func (v SSAValidator) Validate(ssa string) (SSA, error) {
+	t, err := v.parser.Parse(ssa, v.pubKeyLookup)
 	if err != nil {
 		return SSA{}, err
 	}
-	return SSA{SSAPayload: SSAPayload{Issuer: t.Claims.(jwt.StandardClaims).Issuer}}, nil
+	tkmap, ok := t.Claims.(jwt.MapClaims)
+	if !ok {
+		return SSA{}, errors.New("unable to cast jwt.Claims to jwt.MapClaims")
+	}
+	return SSA{SSAPayload: SSAPayload{Issuer: tkmap["iss"].(string)}}, nil
 }
