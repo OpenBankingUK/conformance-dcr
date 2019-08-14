305ea35b81c8591aec6e40d6bb172cf1c4a23ddc
Conor Hackett <chackett1@gmail.com>
2019-08-14 11:46:36 +0100

    Remove MATLSClient struct for nowc

--- pkg/http/client.go
+++ pkg/http/client.go
@@ -7,17 +7,13 @@ import (
 	"net/http"
 )
 
-type MATLSClient struct {
-	http.Client
-}
-
 // NewMATLSClient creates a new http client that is configured for Mutually Authenticated TLS. `insecureSkipVerify` can be set
 // to true if host certificates are not to be validated against a local trusted list of CA certificates. `caCerts` is an
 // optional list of root CA certificates that can be used to validate host certificates. Can be set to nil if not required.
-func NewMATLSClient(certPEMBlock, keyPEMBlock []byte, insecureSkipVerify bool, caCerts []byte) (MATLSClient, error) {
+func NewMATLSClient(certPEMBlock, keyPEMBlock []byte, insecureSkipVerify bool, caCerts []byte) (*http.Client, error) {
 	crt, err := tls.X509KeyPair(certPEMBlock, keyPEMBlock)
 	if err != nil {
-		return MATLSClient{}, errors.Wrap(err, "tls.X509KeyPair")
+		return nil, errors.Wrap(err, "tls.X509KeyPair")
 	}
 
 	tlsConfig := &tls.Config{
@@ -36,8 +32,5 @@ func NewMATLSClient(certPEMBlock, keyPEMBlock []byte, insecureSkipVerify bool, c
 	tlsConfig.BuildNameToCertificate()
 	transport := &http.Transport{TLSClientConfig: tlsConfig}
 
-	result := MATLSClient{}
-	result.Client = http.Client{Transport: transport}
-
-	return result, nil
+	return &http.Client{Transport: transport}, nil
 }
