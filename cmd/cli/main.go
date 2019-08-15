d2c4c502b7523502fbd70be0d6a215352a6d43c6
Diego Mariani <diegomariani.com@gmail.com>
2019-08-15 08:10:03 +0000

    Merged in feature/REFAPP-876 (pull request #15)
    feature/REFAPP-876: SSA / Config file required for DCR
    
    * updates gitignore
    
    * adds minimal config file
    
    * remove vscode from gitignore
    
    * adds example of ssa
    
    * fix lint issues
    
    * use just flag
    
    * go mod tidy move cfg to main
    
    * using flag.Usage instead of log fatal
    
    * check iotuil readall err
    
    * move flag out of func
    
    * adds err
    
    Approved-by: José Pinto <bluecor@gmail.com>
    Approved-by: Conor Hackett <chackett1@gmail.com>

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"bitbucket.org/openbankingteam/conformance-dcr/pkg/compliant"
 )
 
+type Config struct {
+	WellknownEndpoint string `json:"wellknown_endpoint"`
+	SSA               string `json:"ssa"`
+}
+
+func loadConfig(configFilePath string) (Config, error) {
+	var cfg Config
+	f, err := os.Open(configFilePath)
+	if err != nil {
+		return cfg, fmt.Errorf("unable to open config file %s, %v", configFilePath, err)
+	}
+	defer f.Close()
+	rawCfg, err := ioutil.ReadAll(f)
+	if err != nil {
+		return cfg, fmt.Errorf("unable to read config file contents %v", err)
+	}
+	if err := json.NewDecoder(bytes.NewBuffer(rawCfg)).Decode(&cfg); err != nil {
+		return cfg, fmt.Errorf("unable to json decode file contents, %v", err)
+	}
+	return cfg, nil
+}
+
 func main() {
 	fmt.Println("Dynamic Client Registration Conformance Tool cli")
-
-	scenarios := compliant.NewOzoneDCR31()
+	var configFilePath string
+	flag.StringVar(&configFilePath, "config-path", "", "Config file path")
+	flag.Parse()
+	if configFilePath == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	cfg, err := loadConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("unable to load config: %v", err)
+	}
+	scenarios := compliant.NewDCR31(cfg.WellknownEndpoint)
 	tester := compliant.NewVerboseTester()
 
 	passes := tester.Compliant(scenarios)
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/stretchr/testify v1.3.0
 )
