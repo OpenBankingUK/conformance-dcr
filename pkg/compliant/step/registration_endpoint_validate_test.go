2872a7930462c729fa3cbe7261ba37a69d5c1934
Diego <x@diego.pizza>
2019-08-30 14:17:53 +0100

    update test

--- pkg/compliant/step/registration_endpoint_validate_test.go
+++ pkg/compliant/step/registration_endpoint_validate_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewValidateRegistrationEndpoint_Run_ReturnsSuccessfullResult(t *testing.T) {
+func TestNewValidateRegistrationEndpoint_ReturnsSuccessfullResult(t *testing.T) {
 	url := "http://x.org/api/register"
 	registrationEndpointStep := NewValidateRegistrationEndpoint(&url)
 	result := registrationEndpointStep.Run(NewContext())
@@ -14,17 +14,19 @@ func TestNewValidateRegistrationEndpoint_Run_ReturnsSuccessfullResult(t *testing
 	assert.Equal(t, result.Name, "Registration Endpoint Validate")
 }
 
-func TestNewValidateRegistrationEndpoint_Run_ReturnsFailureResultOnInvalidEndpoint(t *testing.T) {
+func TestNewValidateRegistrationEndpoint_ReturnsFailureResultOnInvalidEndpoint(t *testing.T) {
 	url := "foo/bar"
 	registrationEndpointStep := NewValidateRegistrationEndpoint(&url)
 	result := registrationEndpointStep.Run(NewContext())
 	assert.False(t, result.Pass)
 	assert.Equal(t, result.Name, "Registration Endpoint Validate")
+	assert.Contains(t, result.FailReason, "registration endpoint foo/bar is invalid")
 }
 
-func TestNewValidateRegistrationEndpoint_Run_ReturnsFailureResultOnBlankEndpoint(t *testing.T) {
+func TestNewValidateRegistrationEndpoint_ReturnsFailureResultOnBlankEndpoint(t *testing.T) {
 	registrationEndpointStep := NewValidateRegistrationEndpoint(nil)
 	result := registrationEndpointStep.Run(NewContext())
 	assert.False(t, result.Pass)
 	assert.Equal(t, result.Name, "Registration Endpoint Validate")
+	assert.Contains(t, result.FailReason, "registration endpoint is missing")
 }
