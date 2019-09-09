b7a34dea04fdb475c8f544b3d811d25bdff668e7
Diego <x@diego.pizza>
2019-09-09 15:59:06 +0100

    adds unit tests

--- pkg/compliant/step/client_retrieve_with_invalid_token_test.go
+++ pkg/compliant/step/client_retrieve_with_invalid_token_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"bitbucket.org/openbankingteam/conformance-dcr/pkg/compliant/client"
+	"bitbucket.org/openbankingteam/conformance-dcr/pkg/compliant/openid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -35,4 +36,53 @@ func TestNewClientRetrieveWithInvalidToken(t *testing.T) {
 	// assert that response in now in ctx
 	_, err := ctx.GetResponse("responseCtxKey")
 	assert.NoError(t, err)
+}
+
+func TestNewClientRetrieveWithInvalidToken_HandlesMakeRequestError(t *testing.T) {
+	ctx := NewContext()
+	ctx.SetClient("clientKey", client.NewClient(clientID, clientSecret))
+	step := NewClientRetrieveWithInvalidToken("responseCtxKey", string(0x7f), "clientKey", &http.Client{})
+
+	result := step.Run(ctx)
+
+	assert.False(t, result.Pass)
+	assert.Equal(
+		t,
+		"unable to make request: parse \u007f/foo: net/url: invalid control character in URL",
+		result.FailReason,
+	)
+}
+
+func TestNewClientRegisterWithInvalidToken_HandlesExecuteRequestError(t *testing.T) {
+	ctx := NewContext()
+	ctx.SetClient("clientKey", client.NewClient(clientID, clientSecret))
+	step := NewClientRetrieveWithInvalidToken("responseCtxKey", "localhost", "clientKey", &http.Client{})
+
+	result := step.Run(ctx)
+
+	assert.False(t, result.Pass)
+	assert.Equal(
+		t,
+		"unable to call endpoint localhost/foo: Get localhost/foo: unsupported protocol scheme \"\"",
+		result.FailReason,
+	)
+}
+
+func TestNewClientRegisterWithInvalidToken_HandlesErrorForClientNotFound(t *testing.T) {
+	ctx := NewContext()
+	registrationEndpoint := string(0x7f)
+	ctx.SetOpenIdConfig("openIdConfigCtxKey", openid.Configuration{
+		RegistrationEndpoint: &registrationEndpoint,
+		TokenEndpoint:        "",
+	})
+	step := NewClientRetrieveWithInvalidToken("responseCtxKey", "localhost", "clientKey", &http.Client{})
+
+	result := step.Run(ctx)
+
+	assert.False(t, result.Pass)
+	assert.Equal(
+		t,
+		"unable to find client clientKey in context: key not found in context",
+		result.FailReason,
+	)
 }
