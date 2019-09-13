25a295d295d09732e32bfc4e12d15c923108dc08
Conor Hackett <chackett1@gmail.com>
2019-09-13 14:55:07 +0100

    cleanup up update checking

--- pkg/version/check.go
+++ pkg/version/check.go
@@ -1,33 +1,16 @@
 package version
 
 import (
-"encoding/json"
-"fmt"
-"io/ioutil"
-"net/http"
-"regexp"
-"sort"
-"strings"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sort"
 	"time"
 
-	hashiVer "github.com/hashicorp/go-version"
+	goversion "github.com/hashicorp/go-version"
 
-"github.com/pkg/errors"
-)
-
-//  Checker returns the semantic version (see http://semver.org).
-const (
-	// Checker must conform to the format expected, major, minor and patch.
-	major = "1"
-	minor = "1"
-	patch = "18"
-	// Checker is the full string version of Conformance Suite.
-	FullVersion = major + "." + minor + "." + patch
-	// VersionPrerelease is pre-release marker for the version. If this is "" (empty string)
-	// then it means that it is a final release. Otherwise, this is a pre-release
-	// such as "alpha", "beta", "rc1", etc.
-	Prerelease             = ""
-	BitBucketAPIRepository = "https://api.bitbucket.org/2.0/repositories/openbankingteam/conformance-suite/refs/tags"
+	"github.com/pkg/errors"
 )
 
 // Checker defines functionality to reason about the current version of the software and if updates are available
@@ -63,11 +46,11 @@ type TagsAPIResponse struct {
 }
 
 func (t Tag) LessThan(subject string) bool {
-	tv, err := hashiVer.NewVersion(t.Name)
+	tv, err := goversion.NewVersion(t.Name)
 	if err != nil {
 		return false
 	}
-	sv, err := hashiVer.NewVersion(subject)
+	sv, err := goversion.NewVersion(subject)
 	if err != nil {
 		return false
 	}
@@ -95,61 +78,11 @@ func getTags(body []byte) (*TagsAPIResponse, error) {
 	return s, err
 }
 
-// GetHumanVersion composes the parts of the version in a way that's suitable
-// for displaying to humans.
-func (v BitBucket) GetHumanVersion() string {
-	version := "v" + FullVersion
-	release := Prerelease
-
-	if release != "" {
-		if !strings.HasSuffix(version, "-"+release) {
-			// if we tagged a prerelease version then the release is in the version already.
-			version += fmt.Sprintf("-%s", release)
-		}
-	}
-	return version
-}
-
-// VersionFormatter takes a string version number and returns just the numeric parts.
-// This function is used when trying to compare two string versions that 'could'
-// have non numerical properties.
-func (v BitBucket) VersionFormatter(version string) (string, error) {
-	const maxByte = 1<<8 - 1
-	vo := make([]byte, 0, len(version)+8)
-	j := -1
-	for i := 0; i < len(version); i++ {
-		b := version[i]
-		if '0' > b || b > '9' {
-			vo = append(vo, b)
-			j = -1
-			continue
-		}
-		if j == -1 {
-			vo = append(vo, 0x00)
-			j = len(vo) - 1
-		}
-		if vo[j] == 1 && vo[j+1] == '0' {
-			vo[j+1] = b
-			continue
-		}
-		if vo[j]+1 > maxByte {
-			return "", fmt.Errorf("VersionOrdinal: invalid version")
-		}
-		vo = append(vo, b)
-		vo[j]++
-	}
-	// Regex to remove all (non numeric OR period).
-	reg := regexp.MustCompile("[^0-9.]")
-	processedString := reg.ReplaceAllString(string(vo), "")
-
-	return processedString, nil
-}
-
-// UpdateWarningVersion takes a version number and checks it against the
+// UpdateCheck checks the current version against the
 // latest tag version on Bitbucket, if a newer version is found it
 // returns a message and bool value that can be used to inform a user
 // a newer version is available for download.
-func (v BitBucket) UpdateWarningVersion(version string) (string, bool, error) {
+func (v BitBucket) UpdateCheck() (string, bool, error) {
 	// A default message that can be presented to an end user.
 	errorMessageUI := "Version check is unavailable at this time."
 
@@ -177,16 +110,16 @@ func (v BitBucket) UpdateWarningVersion(version string) (string, bool, error) {
 
 		err = resp.Body.Close()
 		if err != nil {
-			return errorMessageUI, false, errors.Wrap(err, "error on update warning version")
+			return errorMessageUI, false, errors.Wrap(err, "close http response body")
 		}
 
 		s, err := getTags(body)
 		if err != nil {
-			return errorMessageUI, false, errors.Wrap(err, "error on update warning version")
+			return errorMessageUI, false, errors.Wrap(err, "getTags")
 		}
 
 		if len(s.TagList) == 0 {
-			return errorMessageUI, false, fmt.Errorf("no Tags found")
+			return errorMessageUI, false, errors.New("no Tags found")
 		}
 
 		// Convert the list of tags to tagList and sort
@@ -196,25 +129,18 @@ func (v BitBucket) UpdateWarningVersion(version string) (string, bool, error) {
 		latestTag := tags[len(tags)-1].Name
 
 		// Format version string to compare.
-		versionLocal, err := v.VersionFormatter(version)
-		if err != nil {
-			return errorMessageUI, false, errors.Wrap(err, "error on update warning version")
-		}
-		versionRemote, err := v.VersionFormatter(latestTag)
-		if err != nil {
-			return errorMessageUI, false, errors.Wrap(err, "error on update warning version")
-		}
+		versionLocal, err := goversion.NewVersion(version)
+		versionRemote, err := goversion.NewVersion(latestTag)
 
-		if versionLocal < versionRemote {
-			errorMessageUI = fmt.Sprintf("Version v%s of the Conformance Suite is out-of-date, please update to v%s", versionLocal, versionRemote)
+		if versionLocal.LessThan(versionRemote) {
+			errorMessageUI = fmt.Sprintf("Version %s of the this tool is out of date, please update to %s", versionLocal, versionRemote)
 			return errorMessageUI, true, nil
 		}
 		// If local and remote version match or is higher then return false update flag.
-		if versionLocal >= versionRemote {
-			errorMessageUI = fmt.Sprintf("Conformance Suite is running the latest version %s", v.GetHumanVersion())
+		if versionLocal.GreaterThanOrEqual(versionRemote) {
+			errorMessageUI = fmt.Sprintf("This tool is running the latest version %s", versionLocal)
 			return errorMessageUI, false, nil
 		}
-
 	} else {
 		// handle anything else other than 200 OK.
 		return "", false, fmt.Errorf("HTTP status %d received", resp.StatusCode)
