8b62668c712bd81b76cc92100183bfd332719692
José Pinto <bluecor@gmail.com>
2019-09-16 09:08:21 +0000

    Merged in hotfix/merge-conflicts (pull request #39)
    fixed merge conflicts
    
    Approved-by: Diego Mariani <diegomariani.com@gmail.com>

--- pkg/compliant/client/client_test.go
+++ pkg/compliant/client/client_test.go
@@ -2,19 +2,23 @@ package client
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestClientBasic(t *testing.T) {
-	client := NewClientBasic("id", "token")
+	client := NewClientBasic("id", "token", "secret")
 
+	request, err := client.CredentialsGrantRequest()
+	require.NoError(t, err)
 	assert.Equal(t, "id", client.Id())
-	assert.Equal(t, "Basic aWQ6dG9rZW4=", client.Token())
+	assert.Equal(t, "Basic aWQ6c2VjcmV0", request.Header.Get("Authorization"))
 }
 
 func TestNoClient(t *testing.T) {
 	client := NewNoClient()
 
+	_, err := client.CredentialsGrantRequest()
+	require.NoError(t, err)
 	assert.Equal(t, "", client.Id())
-	assert.Equal(t, "", client.Token())
 }
